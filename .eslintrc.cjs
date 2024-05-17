adaee71c622c4762474ef0e66d6a9e07ff5e5ae7
KevinKasozi <Kevinkasozi@gmail.com>
2024-05-17 11:11:32 +0000

    Fix linting issues

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,22 +1,40 @@
 module.exports = {
+
   root: true,
-  env: { browser: true, es2020: true },
-  plugins: ['security'],
+
+  env: {
+    browser: true, 
+    es2020: true
+  },
+
+  plugins: ['security', 'react-refresh'], // Plugins consolidated
+
   extends: [
     'eslint:recommended',
-    'plugin:security/recommended',
+    'plugin:security/recommended', 
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    'plugin:react-hooks/recommended'
   ],
+
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+
+  settings: {
+    react: {
+      version: '18.2' 
+    }
+  }, 
+
   rules: {
     'react-refresh/only-export-components': [
       'warn',
-      { allowConstantExport: true },
-    ],
-  },
+      { allowConstantExport: true }
+    ]
+  }
+
 }
